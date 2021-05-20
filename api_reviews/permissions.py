c891794ae44b7446fb4155be75c292e339afed90
Дмитрий <flow.hack.com@gmail.com>
2021-05-20 22:41:56 +0400

    initialize

--- api_reviews/permissions.py
+++ api_reviews/permissions.py
@@ -5,7 +5,6 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS or request.user.is_staff:
             return True
-            
         return False
 
 
